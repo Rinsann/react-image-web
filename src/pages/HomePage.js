79c23421f77bb3b6ed6f26fefad62aa6d9453f37
Rin <ftaocoder@gmail.com>
2024-01-02 21:26:51 +0800

    more images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,14 +7,31 @@ import Picture from '../components/Picture';
 const HomePage = () => {
   let [input,setInput] = useState("");
   let [data,setData] = useState(null);
+  let [page,setPage] = useState(1);
+  let [currentSearch,setCurrentSearch] = useState("");
 
   const auth  = "URFWUp7CORPNLMLr1bKuU7HZBFEd5zH45AqJUD5kQ8ENSbi72olUCFeJ";
-  const initialURL = "https://api.pexels.com/v1/curated?page=1";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
   const search = async (url) => {
     let result = await axios.get(url,{headers:{Authorization:auth}});
     setData(result.data.photos);
+    setCurrentSearch(input)
+  }
+
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1)
+
+    if(currentSearch === ""){
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    }else{
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page + 1}`
+    }
+
+    let result = await axios.get(newURL,{headers:{Authorization:auth}});
+    setData(data.concat(result.data.photos))
   }
 
   useEffect(()=>{
@@ -30,6 +47,9 @@ const HomePage = () => {
         {data && data.map(d => {
           return <Picture data={d} />
         })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>More Images</button>
       </div>
     </div>
   )
