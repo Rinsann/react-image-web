297b0732b465fc76c81fc2163975edf6f4b8dedb
Rin <ftaocoder@gmail.com>
2024-01-02 14:56:22 +0800

    Search images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,31 @@
-import React ,{useState} from 'react'
+import React ,{useEffect, useState} from 'react'
 import axios from 'axios';
 
 import Search from '../components/Search';
 import Picture from '../components/Picture';
 
 const HomePage = () => {
+  let [input,setInput] = useState("");
   let [data,setData] = useState(null);
 
   const auth  = "URFWUp7CORPNLMLr1bKuU7HZBFEd5zH45AqJUD5kQ8ENSbi72olUCFeJ";
   const initialURL = "https://api.pexels.com/v1/curated?page=1";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
-  const search = async ()=>{
-    let result = await axios.get(initialURL,{headers:{Authorization:auth}});
-    console.log(result)
+  const search = async (url) => {
+    let result = await axios.get(url,{headers:{Authorization:auth}});
     setData(result.data.photos);
   }
 
+  useEffect(()=>{
+    search(initialURL)
+  },[])
+
   return (
     <div style={{minHeight: "100vh"}}>
-      <Search search={search} />
+      <Search search={() => {
+        search(searchURL);
+      }} setInput={setInput} />
       <div className='pictures'>
         {data && data.map(d => {
           return <Picture data={d} />
